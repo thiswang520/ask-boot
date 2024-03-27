eb26204c2f81bad7e08fe7968cf63acbd7e39db9
wangyang <thiswangyang@163.com>
2024-03-28 00:34:29 +0800

    初始化

--- src/main/java/com/ask/controller/TestController.java
+++ src/main/java/com/ask/controller/TestController.java
@@ -1,4 +1,15 @@
 package com.ask.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "*")
 public class TestController {
+    @PostMapping("/login")
+    public String examine(){
+        System.out.print("1111111");
+        return "123";
+    }
+
 }
